6369846ba36c077cf963d1ed53fed737b8ca90ce
Jamie Adkins <jamieadkins95@gmail.com>
2016-10-26 00:32:42 +0100

    Added quick notification count to the UI
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- app/src/main/java/com/jamieadkins/notificationlistener/StartActivity.java
+++ app/src/main/java/com/jamieadkins/notificationlistener/StartActivity.java
@@ -2,15 +2,26 @@ package com.jamieadkins.notificationlistener;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.jamieadkins.notificationlistener.models.CaughtNotification;
 
 /**
  * Created by jamiea on 25/10/16.
  */
 
 public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -26,5 +37,12 @@ public class StartActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE}, 1235);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TextView dataView = (TextView) findViewById(R.id.tv_data);
+        dataView.setText(CaughtNotification.listAll(CaughtNotification.class).size() + "");
     }
 }
